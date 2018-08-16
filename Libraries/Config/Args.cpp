8cd1034c90c91b00d8ff6806043e6eaae789b310
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-08-16 09:49:44 +0200

    Config [FEATURE] Rewrite Args module
    TODO: positional arguments

--- Libraries/Config/Args.cpp
+++ Libraries/Config/Args.cpp
@@ -8,23 +8,43 @@ bool isValidArgArray(const std::string &str) {
 	return std::regex_match (str, std::regex("([a-zA-Z]([\\!\\:])?)+"));
 }
 
+bool hasArgumentFormat(const std::string &str) {
+	return std::regex_match (str, std::regex("-[a-zA-Z]"));
+}
+
 bool Args::isOptionDefined(const std::string &opt) {
 	return arguments.find(opt) != arguments.end();
 }
 
 bool Args::parse(int argc, char *argv[]) {
+	positionals.clear();
+	
 	// Parse arguments
-	for (int i = 0; i < argc; i++) {
+	for (int i = 1; i < argc; i++) {
 		std::string opt(argv[i]);
 		
-		// TODO: Positional argument
-		if (not isOptionDefined(opt)) {
-			Logger::error("Args", opt + " is an unknown argument");
-			return false;
+		Logger::debug("Args", "Parsing argument " + opt);
+		
+		// If it is '-[a-zA-Z]' string, treat it like a argument
+		if (hasArgumentFormat(opt)) {
+			if (not isOptionDefined(opt)) { // And check whether it is in arguments map
+				Logger::error("Args", opt + " is an unknown argument");
+				return false;
+			}
+		}
+		else { // Otherwise treat it like positional argument
+			positionals.push_back(opt);
+			continue;
 		}
 		
+		// Check if it is first occurence of the argument
+		if (arguments[opt].set) {
+			Logger::error("Args", opt + " is already set");
+			return false;
+		}
 		arguments[opt].set = true;
 		
+		// Work out optional values
 		if (arguments[opt].hasValue) {
 			if (i + 1 >= argc) {
 				Logger::error("Args", "Argument " + opt + " requires a value");
@@ -54,7 +74,10 @@ bool Args::setupArguments(const std::string &argarray) {
 	}
 	
 	for (unsigned i = 0; i < argarray.size(); i++) {
-		std::string opt = "-" + argarray[i];
+		std::string opt = "-";
+		opt += argarray[i];
+		
+		Logger::debug("Args", "Dealing with argument " + opt);
 		
 		if (isOptionDefined(opt)) {
 			Logger::error("Args", "Redefinition of argument " + opt);
@@ -67,11 +90,13 @@ bool Args::setupArguments(const std::string &argarray) {
 			char c = argarray[i + 1];
 			
 			if (c == '!') {
+				Logger::debug("Args", "Argument is mandatory");
 				arguments[opt].hasValue = true;
 				arguments[opt].required = true;
 				i++;
 			}
 			else if (c == ':') {
+				Logger::debug("Args", "Argument has a value");
 				arguments[opt].hasValue = true;
 				i++;
 			}
--- Libraries/Config/Args.hpp
+++ Libraries/Config/Args.hpp
@@ -8,36 +8,115 @@
 #include "Item.hpp"
 
 namespace cfg {
+	/**
+	 *  \brief Auxiliary struct for handling argument parsing
+	 */
 	struct Arg {
-		cfg::Item value;
+		cfg::Item value; ///< Value of the argument (if there is one)
 		
-		bool required;
-		bool hasValue;
-		bool set;
+		bool required; ///< Is the argument mandatory?
+		bool hasValue; ///< Has the argument value (always yes if it is required)
+		bool set; ///< Has it been set? (set during parsing)
 	};
 	
+	/**
+	 *  \brief Class for parsing command line arguments
+	 *  
+	 *  \details Only supports short arguments and no argument grouping.
+	 *  Arguments can be mandatory and can have values. There also can be
+	 *  positional arguments as well. These are always accepted by default.
+	 */
 	class Args {
 	protected:
-		std::map<std::string, cfg::Arg> arguments;
+		std::map<std::string, cfg::Arg> arguments; ///< Parsed arguments
+		std::vector<std::string> positionals; ///< Positional arguments
 
-		bool isOptionDefined(const std::string &opt);
+		/**
+		 *  \brief Is option defined in arguments array
+		 *  
+		 *  \param [in] opt Option in form '-L' where L is any alphabetical letter
+		 *  \return TRUE if is
+		 */
+		bool isOptionDefined(const std::string &opt); ///< Whether option is stored in arguments
 		
 	public:
+		/**
+		 *  \brief Parse command line arguments
+		 *  
+		 *  \param [in] argc Argument count
+		 *  \param [in] argv Arguments
+		 *  \return TRUE if parse was successful
+		 *  
+		 *  \pre \ref setupArguments was called
+		 *  
+		 *  \details Positional arguments are by default stored, no questions asked and
+		 *  you can retrieve them with \ref getOtherArguments. Only exceptions
+		 *  are arguments that have a format '-L' which are treated as regular
+		 *  arguments and are checked against setup rules.
+		 */
 		bool parse(int argc, char *argv[]);
 		
+		/**
+		 *  \brief Setup how arguments will be parsed
+		 *  
+		 *  \param [in] argarray String with parsing instructions
+		 *  \return TRUE if setup succeeded
+		 *  
+		 *  \details \p argarray has following format: It can
+		 *  contain any number of nonrepeating alphabetical letters
+		 *  (both minuscule and capital) and each of these letters
+		 *  can be followed by one of the two special symbols ':' or '!'.
+		 *  ':' means that argument has a value, '!' means that argument
+		 *  is mandatory AND have a value.
+		 *  
+		 *  For example following \p argarray "hg:i!" means:
+		 *  program [-h] [-g <value>] <-i <value>> where [] denotes optional
+		 *  items and <> denotes mandatory items.
+		 */
 		bool setupArguments(const std::string &argarray);
 		
+		/**
+		 *  \brief Test if an argument was set during parsing
+		 *  
+		 *  \param [in] argument Single letter identifying the argument
+		 *  \return TRUE if is set, FALSE otherwise
+		 *  
+		 *  \pre \ref parse was called
+		 *  
+		 *  \details If the \p argument is mandatory, then this check is not
+		 *  needed, as long as \ref parse returned TRUE. Use this check prior
+		 *  to calling \ref getArgumentValue on optional arguments.
+		 */
 		bool isSet(const char argument) const {
-			std::string arg = "-" + argument;
-			return arguments.find(arg) != arguments.end();
+			std::string arg = "-";
+			arg += argument;
+			
+			if (arguments.find(arg) == arguments.end()) return false;
+			
+			return arguments.at(arg).set;
 		}
 		
-		const cfg::Item &getArgument(const char argument) const {
-			std::string arg = "-" + argument;
+		/**
+		 *  \brief Get value of a argument
+		 *  
+		 *  \param [in] argument Single letter identifying the argument
+		 *  \return Reference to structure holding the argument value
+		 *  
+		 *  \details Since the reference is returned, always call \ref isSet
+		 *  on optional arguments to prevent exceptions from being thrown.
+		 */
+		const cfg::Item &getArgumentValue(const char argument) const {
+			std::string arg = "-";
+			arg += argument;
 			return arguments.at(arg).value;
 		}
 		
-		//const std::vector<std::string> &getOtherArguments() const { return otherArguments; }
+		/**
+		 *  \brief Get parsed positional arguments
+		 *  
+		 *  \pre \ref parse was called
+		 */
+		const std::vector<std::string> &getPositionalArguments() const { return positionals; }
 	};
 }
 

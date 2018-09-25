bf2896ac7b66bd3fbe1b5185f11811591fd3bfd4
Jakub Neruda <neruda@netcope.com>
2018-09-25 13:19:33 +0200

    Config [INDEV] Rewrite csv parser

--- Libraries/Config/Csv.cpp
+++ Libraries/Config/Csv.cpp
@@ -5,141 +5,107 @@
 
 using cfg::Csv;
 
-enum State { Word, Quote, TestDQuote };
-
-#define EOFc std::char_traits<char>::eof()
-
-class ParserException : public std::exception {
-	virtual const char *what() const noexcept override {
-		return "Unknown parser exception";
-	}
+enum class ParserMode {
+	Start,
+	Quoted,
+	Unquoted
 };
 
-class ParserExceptionEof : public ParserException {
-	virtual const char *what() const noexcept final override {
-		return "EOF was already encountered during parsing";
-	}
-};
+struct ParserSettings {
+	char delimiter;
+	char quote;
+	char newline;
+}
 
-bool Csv::parseLine(char **buffer, std::vector<cfg::Item> &row, int flags, char delimiter, char newline, char quote) {
-	// Test eof, throw except if needed
-	if ((*buffer)[0] == EOFc) throw ParserExceptionEof;
+bool parseToken(const std::string &file, std::string::const_iterator &itr, std::string &token, int flags, ParserSettings settings) {
+	ParserMode mode = ParserMode::Start;
+
+	std::string token;
+
+	while (true) {
+		switch (mode) {
+		case ParserMode::Start:
+			if (*itr == settings.quote) {
+				mode = ParserMode::Quoted;
+			}
+			else {
+				mode = ParserMode::Unquoted;
+			}
+			break;
+			
+		case ParserMode::Quoted:
+			
+			break;
+
+		case ParserMode::Unquoted:
+			break;
+		}
+	}
+	
+	return true;
+}
+
+bool Csv::parseLine(const std::string &file, std::string::const_iterator &itr, std::vector<cfg::Item> &row, int flags, ParserSettings settings) {
+	if (itr == file.end()) return false; // Parsing ended
 
 	row.clear();
 
 	std::string token;
-	int start = 0, last = 0;
-	State state = State::Word;
-	char c;
+
 	// Read tokens until line end
-	while (true) {
-		c = *buffer[last];
-		switch(state) {
-		case State::Word:
-			if (c == delimiter) {
-				tokenToRow(*buffer, start, last, row, quote);
-				start = last + 1;
-			}
-			else if (c == newline) {
-				tokenToRow(*buffer, start, last, row, quote);
-
-				// Last token on line, exit
-				return (*buffer)[last + 1] != EOFc;
-			}
-			else if (c == quote) {
-				state = State::Quote;
-			}
-			else if (c == EOFc) {
-				tokenToRow(*buffer, start, last, row, quote);
-				return false; // No data after this
-			}
-			break;
-
-		case State::Quote:
-			if (c == quote) {
-				state = State::TestDQuote;
-			}
-			break;
-
-		case State::TestDQuote:
-			if (c == quote) {
-				state = State::Quote;
-			}
-			else {
-				last--;
-				state = State::Word;
-			}
-			break;
+	while (parseToken(file, itr, token, flags, settings)) {
+		if (token[0] == token.back() && token[0] == settings.quote) {
+			token = token.substr(1, token.size() - 2);
+			Strings::replaceAll(token, std::string(settings.quote) + std::string(settings.quote), std::string(settings.quote);
 		}
-
-		last++;
+		
+		row.push_back(cfg::Item(token));
 	}
 
-	return false;
-}
-
-void Csv::tokenToRow(const char *buffer, int start, int last, std::vector<cfg::Item> &row, char quote) {
-	std::string token;
-	// Copy buffer substr to token
-	for (int i = start; i < last; i++) {
-		token += (*buffer)[i];
-	}
-	
-	// Remove start/end quotes
-	if (token[0] == quote && token[token.size() - 1] == quote) {
-		token = token.substr(1, token.size() - 2);
-	}
-	
-	// Reduce doublequotes to single quotes
-	//Strings::replaceAll(token, std::string(quote) + std::string(quote), std::string(quote));
-	
-	// Push it to row
-	log.debug("Csv::parseLine", "Deparsed token is: " + token);
-	row.push_back(cfg::Item());
-	row.back() = token;
-	token.clear();
+	return true;
 }
 
 bool Csv::loadFromFile(const std::string &filename, int flags, char delimiter, char newline, char quote) noexcept {
+	std::string file;
+	std::vector<cfg::Item> row;
+
+	// Load file into memory
 	try {
 		std::ifstream load(filename);
-		
-		// Buffer
-		std::vector<cfg::Item> row;
-		char* filebuf;
-		
+
 		// Get size of file
 		load.seekg(0, load.end);
 		unsigned fsize = load.tellg();
 		load.seekg(0, load.begin);
-		
-		// Create buffer and read file into memory
-		filebuf = new char[fsize + 1];
+
+		// Allocate buffer
+		char *filebuf = new char[fsize];
 		if (filebuf == NULL) throw std::bad_alloc;
+
+		// Read file into memory
 		load.read(filebuf, fsize);
-		filebuf[fsize] = char_traits<char>::eof();
-		
-		char *backup = filebuf;
-		
-		// Process headers
-		if (!(flags & Csv::Flags::NoHeaders)) {
-			parseLine(&filebuf, row, flags, delimiter, newline, quote);
-			headers = row;
-		}
-		
-		// Process body
-		while (parseLine(&filebuf, row, flags, delimiter, newline, quote)) {
-			rows.push_back(row);
-		}
-		
-		free(backup); // filebuf is moved
-		
+		file = std::string(filebuf, fsize);
+
 		load.close();
 		load.clear();
 	}
 	catch (std::exception &e) {
 		log.error("Csv::loadFromFile", "Exception: " + std::string(e.what()));
 		return false;
+	}
+	
+	// Parse file
+	auto fileItr = file.begin();
+	
+	// Process headers
+	if (!(flags & Csv::Flags::NoHeaders)) {
+		parseLine(file, fileItr, row, flags, {delimiter, quote, newline});
+		headers = row;
+	}
+	
+	// Process body
+	while (parseLine(file, fileItr, row, flags, {delimiter, quote, newline})) {
+		rows.push_back(row);
 	}
 	
 	return true;

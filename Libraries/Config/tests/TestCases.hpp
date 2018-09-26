ea5e2aec1d04b223622894a198f49d720a5b56a5
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-09-26 13:55:44 +0200

    Config [FEATURE] Add more tests and capabilities

--- Libraries/Config/tests/TestCases.hpp
+++ Libraries/Config/tests/TestCases.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <cassert>
+#include <vector>
 #include "../Config.hpp"
 
 class TestCase {
@@ -11,6 +12,7 @@ public:
 	}
 
 	virtual void run() =0;
+	virtual std::string name() const = 0;
 
 	TestCase() {}
 	virtual ~TestCase() {}
@@ -25,10 +27,7 @@ public:
 		cfg::Csv csv;
 		assume(csv.loadFromFile(data, int(cfg::Csv::Flags::NoHeaders)), "CSV did not loaded file " + data + " correctly");
 		assume(csv.getSize() == 2, "Number of rows should have been 2");
-		
-		/*for (auto row = csv.begin(); row != csv.end(); row++) {
-			assume(row.size() == 3, "Size of each row should be 3");
-		}*/
+
 		int cnt = 0;
 		for (auto row : csv) {
 			assume(row.size() == 3, "Size of row " + std::to_string(cnt) + " should be 3, but is " + std::to_string(row.size()));
@@ -36,5 +35,37 @@ public:
 		}
 	}
 	
+	virtual std::string name() const final override {
+		return "TestLoadValidCSV(" + data + ")";
+	}
+
 	TestLoadValidCSV(const std::string &filename) { data = filename; }
+};
+
+class TestValidCSVData : public TestCase {
+protected:
+	std::string data;
+	const std::vector<std::vector<std::string>> refValues;
+
+public:
+	virtual void run() final override {
+		cfg::Csv csv;
+		assume(csv.loadFromFile(data, int(cfg::Csv::Flags::NoHeaders)), "CSV did not loaded file " + data + " correctly");
+		
+		assume(csv.getSize() == refValues.size(), "Number of rows in csv does not match referenceValues");
+		
+		for (unsigned i = 0; i < csv.getSize(); i++) {
+			assume(csv[i].size() == refValues[i].size(), "Number of columns on row " + std::to_string(i) + " in csv (" + std::to_string(csv[i].size()) + ") does not match reference data (" + std::to_string(refValues[i].size()) + ").");
+			
+			for (unsigned p = 0; p < csv[i].size(); p++) {
+				assume(csv[i][p].asString() == refValues[i][p], "Value '" + csv[i][p].asString() + "' from csv does not match '" + refValues[i][p] + "' from reference data.");
+			}
+		}
+	}
+
+	virtual std::string name() const final override {
+		return "TestValidCSVData(" + data + ")";
+	}
+
+	TestValidCSVData(const std::string &filename, const std::vector<std::vector<std::string>> &correctValues) : refValues(correctValues) { data = filename; }
 };
--- Libraries/Config/tests/main.cpp
+++ Libraries/Config/tests/main.cpp
@@ -7,12 +7,29 @@ int main() {
 		new TestLoadValidCSV("tests/test0.csv"),
 		new TestLoadValidCSV("tests/test1.csv"),
 		new TestLoadValidCSV("tests/test2.csv"),
-		new TestLoadValidCSV("tests/test3.csv")
+		new TestLoadValidCSV("tests/test3.csv"),
+		new TestLoadValidCSV("tests/test4.csv"),
+		new TestLoadValidCSV("tests/test5.csv"),
+		new TestValidCSVData("tests/test0.csv", {
+			{"aaa", "bbb", "ccc"}, { "ddd", "eee", "fff" }
+		}),
+		new TestValidCSVData("tests/test1.csv", {
+			{"aaa", "bbb", "ccc"}, { "ddd", "eee", "fff" }
+		}),
+		new TestValidCSVData("tests/test2.csv", {
+			{"aaa", "bbb", "cc\nc"}, { "ddd", "eee", "fff" }
+		}),
+		new TestValidCSVData("tests/test3.csv", {
+			{"aaa", "bbb", "cc,c"}, { "ddd", "eee", "fff" }
+		}),
+		new TestValidCSVData("tests/test4.csv", {
+			{"aaa", "bbb", "cc\"c"}, { "ddd", "eee", "fff" }
+		}),
 	};
 
 	int cnt = 1;
 	for (auto test : cases) {
-		std::cout << "TEST " << cnt << "/" << cases.size() << "\n";
+		std::cout << "TEST " << cnt << "/" << cases.size() << " - " << test->name() << "\n";
 
 		try {
 			test->run();

939dabf0e26d38bbb13ed470249d682542524810
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-10-05 17:48:02 +0200

    LifeCounter [FEATURE] Add method RenderButtonArray

--- AppJS/LifeCounter/src/template.js
+++ AppJS/LifeCounter/src/template.js
@@ -20,35 +20,39 @@
 	this.action = action;
 }
 
-'static'; function RenderToolbarTemplate(parentCanvas, buttons, cacheID) {
-	var canvas = parentCanvas.add(0, 0.9, 1, 0.1);
-	canvas.setColor(SYSCOLOR_TOOLBAR);
-	
-	var BUTTON_WIDTH = 1 / buttons.length;
-	
-	if (TOOLBAR_TEMPLATE_FONT_SIZE_CACHE[cacheID] == null) {
+'static'; function RenderButtonArray(canvas, buttons, x, y, w, h, cacheID) {
+	var BUTTON_WIDTH = w / buttons.length;
+
+	if (GLOBAL_FONT_SIZE_CACHE[cacheID] == null) {
 		var longestStr = '';
 		for (var i = 0; i < buttons.length; i++) {
 			longestStr = maxStr(longestStr, buttons[i].label);
 		}
-		
-		TOOLBAR_TEMPLATE_FONT_SIZE_CACHE[cacheID] = GetOptimalFontSize(
+
+		GLOBAL_FONT_SIZE_CACHE[cacheID] = GetOptimalFontSize(
 			longestStr,
 			canvas.width * BUTTON_WIDTH,
-			canvas.height
+			canvas.height * h
 		);
 	}
-	
+
 	for (var i = 0; i < buttons.length; i++) {
+		if (buttons[i] == null) continue;
 		(function(p) {
-			var opt = canvas.add(i * BUTTON_WIDTH, 0, BUTTON_WIDTH, 1, 'button');
+			var opt = canvas.add(x + i * BUTTON_WIDTH, y, BUTTON_WIDTH, h, 'button');
 			opt.dom.addEventCallback('click', buttons[p].action);
-			opt.dom.style.fontSize = TOOLBAR_TEMPLATE_FONT_SIZE_CACHE[cacheID] + 'px';
+			opt.dom.style.fontSize = GLOBAL_FONT_SIZE_CACHE[cacheID] + 'px';
 			opt.setText(buttons[p].label);
 		}(i));
 	}
 }
 
+'static'; function RenderToolbarTemplate(parentCanvas, buttons, cacheID) {
+	var canvas = parentCanvas.add(0, 1 - TEMPLATE_TOOLBAR_HEIGHT, 1, TEMPLATE_TOOLBAR_HEIGHT);
+	canvas.setColor(SYSCOLOR_TOOLBAR);
+	
+	RenderButtonArray(canvas, buttons, 0, 0, 1, 1, cacheID);
+}
 'static'; function GetDrawingCanvas(core, hasHeader) {
 	var HEADER_OFFSET = hasHeader ? TEMPLATE_HEADER_HEIGHT : 0;
 	var result = core.add(0, HEADER_OFFSET, 1, 1 - HEADER_OFFSET - TEMPLATE_TOOLBAR_HEIGHT);

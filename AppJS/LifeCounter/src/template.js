1274f803ca336e7f0342ee1279b34d16501f5dc2
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-10-06 12:15:28 +0200

    LifeCounter [DOCS] Document template.js

--- AppJS/LifeCounter/src/template.js
+++ AppJS/LifeCounter/src/template.js
@@ -12,6 +12,20 @@
 	this.action = action;
 }
 
+/**
+ *  @brief Render array of buttons in parent canvas
+ *  
+ *  @param [in] canvas Destination canvas to render
+ *  @param [in] buttons Array of ButtonTemplates
+ *  @param [in] x X coordinate within canvas
+ *  @param [in] y Y coordinate within canvas
+ *  @param [in] w Width of button array within canvas
+ *  @param [in] h Height of button array within canvas
+ *  @param [in] cacheID ID to cache with fontsizes
+ *  
+ *  @details If any of the fields of \p buttons is null, that
+ *  element will be skipped and blank space will be rendered instead.
+ */
 'static'; function RenderButtonArray(canvas, buttons, x, y, w, h, cacheID) {
 	var BUTTON_WIDTH = w / buttons.length;
 
@@ -39,6 +53,18 @@
 	}
 }
 
+/**
+ *  @brief Render app standard toolbar
+ *  
+ *  @param [in] parentCanvas Parent canvas that should contain toolbar
+ *  @param [in] buttons Array of ButtonTemplates
+ *  @param [in] cacheID ID for fontsize cache
+ *  
+ *  @details This function will create new container element positioned appropriately within parent canvas,
+ *  set its style and background color and then render buttons with \ref RenderButtonArray.
+ *  
+ *  Use this in conjunction with \ref RenderViewHeaderTemplate and \ref GetDrawingCanvas.
+ */
 'static'; function RenderToolbarTemplate(parentCanvas, buttons, cacheID) {
 	var canvas = parentCanvas.add(0, 1 - TEMPLATE_TOOLBAR_HEIGHT, 1, TEMPLATE_TOOLBAR_HEIGHT);
 	canvas.setColor(SYSCOLOR_TOOLBAR);
@@ -46,6 +72,17 @@
 	RenderButtonArray(canvas, buttons, 0, 0, 1, 1, cacheID);
 }
 
+/**
+ *  @brief Render header of the view
+ *  
+ *  @param [in] canvas Parent canvas where header should be rendered
+ *  @param [in] label Label of the header
+ *  
+ *  @details This creates new container within canvas, sets the text label, sets background color.
+ *  Container will be positioned appropriately within parent canvas. All headers use the same cache ID.
+ *  
+ *  Use this in conjunction with \ref RenderToolbarTemplate and \ref GetDrawingCanvas.
+ */
 'static'; function RenderViewHeaderTemplate(canvas, label) {
 	var header = canvas.add(0, 0, 1, TEMPLATE_HEADER_HEIGHT);
 	
@@ -59,6 +96,15 @@
 	header.setColor(SYSCOLOR_HEADER);
 }
 
+/**
+ *  @brief Get canvas for drawing view context (header and toolbar free)
+ *  
+ *  @param [in] core Core canvas where drawing canvas will be embedded
+ *  @param [in] hasHeader Whether header is present in view
+ *  @return Resulting canvas
+ *  
+ *  @details Use this in conjunction with \ref RenderToolbarTemplate and \ref RenderViewHeaderTemplate.
+ */
 'static'; function GetDrawingCanvas(core, hasHeader) {
 	var HEADER_OFFSET = hasHeader ? TEMPLATE_HEADER_HEIGHT : 0;
 	var result = core.add(0, HEADER_OFFSET, 1, 1 - HEADER_OFFSET - TEMPLATE_TOOLBAR_HEIGHT);

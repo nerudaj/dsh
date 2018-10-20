7ac63a1027ce14395a356b29ff5add4767acad2b
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-10-20 18:04:45 +0200

    jsbloat [FEATURE] Add new rule to properly minify code

--- Utilities/jsbloat/Main.cpp
+++ Utilities/jsbloat/Main.cpp
@@ -47,6 +47,7 @@ void replaceIds(const std::unordered_map<std::string, std::string> &ids, std::st
 			replaceAll(file, id.first + "(", id.second + "(");
 			replaceAll(file, id.first + " ", id.second + " ");
 			replaceAll(file, id.first + ",", id.second + ",");
+			replaceAll(file, id.first + ";", id.second + ";");
 			replaceAll(file, " " + id.first + ".", " " + id.second + ".");
 			replaceAll(file, "\n" + id.first + ".", "\n" + id.second + ".");
 		}

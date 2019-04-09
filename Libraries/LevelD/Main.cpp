0e020b8fd59ac66c06b377ac3ff8490ec2ebd969
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-04-09 14:08:27 +0200

    [TESTS] Make summary optional

--- Libraries/LevelD/Main.cpp
+++ Libraries/LevelD/Main.cpp
@@ -1,6 +1,7 @@
 #include "LevelD.hpp"
 #include "Tests/Test.hpp"
 #include <iostream>
+#include <Config.hpp>
 
 using std::string;
 using std::vector;
@@ -9,7 +10,20 @@ void assume(bool cond, const std::string &str) {
     if (!cond) throw std::runtime_error(str);
 }
 
+void printHelp(const std::string &programName);
+
 int main(int argc, char *argv[]) {
+    cfg::Args args("hs");
+    if (!args.parse(argc, argv)) {
+        printHelp(argv[1]);
+        return 1;
+    }
+
+    if (args.isSet('h')) {
+        printHelp(argv[1]);
+        return 0;
+    }
+
     vector<Test*> tests = {
         new TestMetadata({}),
         new TestMetadata({{"map01", "Hangar", "doomista", "dsc", 10}, {}, {}, {}}),
@@ -40,11 +54,31 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    std::cout << std::endl;
-    std::cout << "SUMMARY:" << std::endl;
-    std::cout << "Total  : " << cnt << std::endl;
-    std::cout << "Passed : " << success << std::endl;
-    std::cout << "Failed : " << failure << std::endl;
+    if (args.isSet('s')) {
+        std::cout << std::endl;
+        std::cout << "SUMMARY:" << std::endl;
+        std::cout << "Total  : " << cnt << std::endl;
+        std::cout << "Passed : " << success << std::endl;
+        std::cout << "Failed : " << failure << std::endl;
+    }
 
     return failure;
+}
+
+void printHelp(const std::string &programName) {
+    std::cout << "Usage:" << std::endl;
+    std::cout << "\t" << programName << " [-h] [-s]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Parameters:" << std::endl;
+    vector<std::pair<string, string>> params = {
+        {"-h", "Prints help" },
+        {"-s", "Print test summary"}
+    };
+
+    for (auto &param : params) {
+        std::cout << "\t" << param.first << "  " << param.second << std::endl;
+    }
+
+    std::cout << std::endl;
 }

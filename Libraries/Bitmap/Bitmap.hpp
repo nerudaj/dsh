375dd86d1a5ac89649cf57896614396d5c334ff4
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-09-11 17:15:07 +0200

    Add doxydocs for bitmap

--- Libraries/Bitmap/Bitmap.hpp
+++ Libraries/Bitmap/Bitmap.hpp
@@ -9,15 +9,31 @@ namespace bmp {
 		uint8_t green;
 		uint8_t blue;
 
+		/**
+		 *  \brief Grayscale constructor for Color
+		 */
 		Color(uint8_t c) : red(c), green(c), blue(c) {}
+
+		/**
+		 *  \brief Standard 3 color constructor for Color
+		 */
 		Color(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b) {}
 	};
 
+	/**
+	 *  \brief For standard 256 colors bitmap, Palette defines which colors are available
+	 */
 	struct Palette {
 		Color colors[256];
 
+		/**
+		 *  \brief Generates a grayscale palette
+		 */
 		static Palette getGrayscalePalette();
 
+		/**
+		 *  \brief Generates standard UNIX 8bit palette
+		 */
 		static Palette getUnixPalette();
 	};
 
@@ -29,8 +45,24 @@ namespace bmp {
 		Palette palette;
 	
 	public:
+		/**
+		 *  \brief Create empty bitmap canvas
+		 * 
+		 *  \param[in]  width   Width of the canvas
+		 *  \param[in]  height  Height of the canvas
+		 * 
+		 *  Canvas will be filled with first references to first
+		 *  color in the palette. Changing the palette with \ref
+		 *  setPalette will thus change the color of the canvas
+		 */
 		void create(unsigned width, unsigned height);
 
+		/**
+		 *  \brief Initializes the object
+		 * 
+		 *  Canvas is empty width zero width and height.
+		 *  Palette is set to grayscale
+		 */
 		Bitmap();
 	};
 };

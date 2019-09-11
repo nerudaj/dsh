0c9963954673f9772d61666621221265b4f0fc90
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-09-11 17:08:16 +0200

    Add Palette.cpp
    Implement getGrayscalePalette and getUnixPalette

--- Libraries/Bitmap/Bitmap.hpp
+++ Libraries/Bitmap/Bitmap.hpp
@@ -8,10 +8,17 @@ namespace bmp {
 		uint8_t red;
 		uint8_t green;
 		uint8_t blue;
+
+		Color(uint8_t c) : red(c), green(c), blue(c) {}
+		Color(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b) {}
 	};
 
 	struct Palette {
 		Color colors[256];
+
+		static Palette getGrayscalePalette();
+
+		static Palette getUnixPalette();
 	};
 
 	class Bitmap {

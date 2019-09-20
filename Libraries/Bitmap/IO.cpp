f8d5ebb8cc5c4c1ce5625340752e6d1d357bc2a9
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-09-20 22:14:58 +0200

    libbitmap: [BUFIX] Fix bad pixel save/oad

--- Libraries/Bitmap/IO.cpp
+++ Libraries/Bitmap/IO.cpp
@@ -8,6 +8,8 @@ void IO::savePaletteToStream(const bmp::Palette &palette, std::ofstream &save) {
 }
 
 void IO::savePixelsToStream(const std::vector<uint8_t> &pixels, uint32_t width, std::ofstream &save) {
+	if (width == 0) return;
+	
 	uint32_t height = pixels.size() / width;
 	for (uint32_t cnt = 0, y = height - 1; cnt < height; cnt++, y--) {
 		for (uint32_t x = 0; x < width; x++) {
@@ -39,6 +41,13 @@ std::vector<uint8_t> IO::loadPixelsFromStream(std::ifstream &load, uint32_t widt
 	for (uint32_t y = 0; y < height; y++) {
 		for (uint32_t x = 0; x < width; x++) {
 			load.read((char*)(&result[(height - 1 - y) * width + x]), sizeof(uint8_t));
+		}
+		
+		if (width % 4 != 0) {
+			char dummy;
+			for (int i = width % sizeof(uint32_t); i < 4; i++) {
+				load.read(&dummy, sizeof(uint8_t));
+			}
 		}
 	}
 

b6896da4943cd5c28152535012bee0e6449d4294
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-09-26 19:28:31 +0200

    libperlin [TESTS]: Migrate libperlin tests to catch2

--- Libraries/Perlin/tests/Main.cpp
+++ Libraries/Perlin/tests/Main.cpp
@@ -1,19 +1,2 @@
-#include <Test.hpp>
-
-#include "VectorTests.hpp"
-
-int main() {
-	Testrunner runner({
-		new VectorGetNormalizedIndexZeroTest(),
-		new VectorGetNormalizedIndexOneDimTest(2, 10, 2),
-		new VectorGetNormalizedIndexOneDimOverflowTest(2, 1, 0),
-		new VectorGetNormalizedIndexOneDimOverflowTest(10, 7, 3),
-		new VectorGetNormalizedIndexMultiDimTest<2>({{1, 1}}, {{2, 2}}, 3),
-		new VectorGetNormalizedIndexMultiDimTest<2>({{5, 1}}, {{10, 10}}, 15),
-		new VectorGetNormalizedIndexMultiDimTest<2>({{5, 1}}, {{1, 1}}, 0),
-		new VectorGetNormalizedIndexMultiDimTest<3>({{1, 1, 1}}, {{2, 2, 2}}, 7),
-		new VectorGetNormalizedIndexMultiDimTest<4>({{1, 1, 1, 1}}, {{2, 2, 2, 2}}, 15)
-	});
-	
-	return runner.evaluateTestcases(true);
-}
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
---
+++ Libraries/Perlin/tests/VectorTests.cpp
@@ -0,0 +1,61 @@
+#include <catch.hpp>
+#include "../Perlin.hpp"
+#include "../Vector.cpp"
+
+TEST_CASE("getNormalizedIndex zero test", "[Vector]") {
+	perlin::Vector<3> vec({0, 0, 0});
+	int out = vec.getNormalizedIndex({{10, 10, 10}});
+	REQUIRE(out == 0);
+}
+
+TEST_CASE("getNormalizedIndex one dimension test", "[Vector]") {
+	SECTION("2-10") {
+		perlin::Vector<1> vec({2});
+		int out = vec.getNormalizedIndex({{10}});
+		REQUIRE(out == 2);
+	}
+
+	SECTION("overflow 2-1") {
+		perlin::Vector<1> vec({2});
+		int out = vec.getNormalizedIndex({{1}});
+		REQUIRE(out == 0);
+	}
+
+	SECTION("overflow 10-7") {
+		perlin::Vector<1> vec({10});
+		int out = vec.getNormalizedIndex({{7}});
+		REQUIRE(out == 3);
+	}
+}
+
+TEST_CASE("getNormalizedIndex multi dim test", "[Vector]") {
+	SECTION("1,1-2,2") {
+		perlin::Vector<2> vec({1, 1});
+		int out = vec.getNormalizedIndex({{2, 2}});
+		REQUIRE(out == 3);
+	}
+
+	SECTION("5,1-10,10") {
+		perlin::Vector<2> vec({5, 1});
+		int out = vec.getNormalizedIndex({{10, 10}});
+		REQUIRE(out == 15);
+	}
+
+	SECTION("5,1-1,1") {
+		perlin::Vector<2> vec({5, 1});
+		int out = vec.getNormalizedIndex({{1, 1}});
+		REQUIRE(out == 0);
+	}
+
+	SECTION("1,1,1-2,2,2") {
+		perlin::Vector<3> vec({1, 1, 1});
+		int out = vec.getNormalizedIndex({{2, 2, 2}});
+		REQUIRE(out == 7);
+	}
+
+	SECTION("1,1,1,1-2,2,2,2") {
+		perlin::Vector<4> vec({1, 1, 1, 1});
+		int out = vec.getNormalizedIndex({{2, 2, 2, 2}});
+		REQUIRE(out == 15);
+	}
+}

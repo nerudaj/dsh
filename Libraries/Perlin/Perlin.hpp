1ed047c053e5a0eeaedf4ac12b7cd92c85326f64
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-10-09 14:27:08 +0200

    Update Perlin.hpp

--- Libraries/Perlin/Perlin.hpp
+++ Libraries/Perlin/Perlin.hpp
@@ -3,6 +3,10 @@
 #include <array>
 #include <vector>
 
+#include "Generate.hpp"
+#include "Math.hpp"
+#include "Convert.hpp"
+
 namespace perlin {
 	template<unsigned Dimensions>
 	using Vector = std::array<int, Dimensions>;
@@ -27,60 +31,50 @@ namespace perlin {
 		std::vector<std::vector<float>> gradients;
 
 		void generateGradients() {
-			/*std::mt19937 generator(SEED);
+			std::mt19937 generator(SEED);
 
 			for (auto &grad : gradients) {
-				grad = generateGradient<Dimensions>(COMPLEXITY, generator);
-			}*/
+				grad = generateGradient(COMPLEXITY, generator, Dimensions);
+			}
 		}
 
 		void initialize(const unsigned dimensions) {
-			
+			gridSize = {12, 12, 12};
+			gridDensity = {3, 3, 3};
+			gradients.resize(Math::getDimensionsProduct(gridSize));
+			generateGradients();
 		}
 
 		float getValueAt(const std::vector<int> &point) const {
-/*
-			// Normalize coord using gridDensity and convert it to float vector (Gradient)
-			Gradient<Dimensions> normalizedCoords = GradientMath::makeGradientFromCoord<Dimensions>(coords, gridDensity);
-
-			// Floor coordinates 
-			Vector<Dimensions> flooredCoords = GradientMath::makeVectorFromGradient<Dimensions>(normalizedCoords);
+			VectorFloat offsetPoint = Convert::vecIntToOffsetedFloat(point, gridDensity);
+			VectorInt flooredPoint = Convert::vecFloatToInt(offsetPoint);
 
 			// Compute lerp factors
-			std::array<float, Dimensions> lerpFactors;
+			VectorFloat lerpFactors(Dimensions);
 			for (unsigned i = 0; i < Dimensions; i++) {
-				lerpFactors[i] = normalizedCoords[i] - static_cast<float>(flooredCoords.getDim(i));
+				lerpFactors[i] = normalizedCoords[i] - static_cast<float>(flooredPoint[i]);
 			}
 
 			// Get gradient normalized index
-			int index = flooredCoords.getNormalizedIndex(gridSize);
+			int index = Math::getSquashedIndex(flooredPoint, gridSize);
 
 			// Compute bounding box
-			auto bounds = GradientMath::getGradientIndices<Dimensions>(index, gridSize);
+			auto bounds = Math::getPointBoundingBox(index, gridSize);
 
 			std::vector<float> dotProducts;
 			for (auto &index : bounds) {
 				auto &gradient = gradients[index];
-				auto vecToPoint = GradientMath::constructGradientFromPoints<Dimensions>(normalizedCoords, gradient);
+				auto gradientLoc = /* TODO */{0, 0, 0};
+				auto vecToPoint = VectorMath::getVectorFromPoints(offsetPoint, gradientLoc);
 
-				dotProducts.push_back(GradientMath::getGradientDotProduct<Dimensions>(vecToPoint, gradient));
+				dotProducts.push_back(VectorMath::getDotProduct<Dimensions>(vecToPoint, gradient));
 			}
 
-*/
-
-			// For each vertex of the bounding box
-			// Get gradient of that vertex
-			// Compute vector from vertex to gcoord
-			// Compute dot product of those two vectors
-
-			// This leads to some sort of reduce algorithm
-			// Gather values from previous step and somehow lerp them using params[0]
-			
-			// Gather values from previous step and somehow lerp them using params[1]
-			
-			// ....
-			
-			// Gather values from previous step and somehow lerp them using params[n]
+			for (unsigned t = 1, skip = 2, f = 0; t > dotProducts.size(); t *= 2, skip *= 2, f++) {
+				for (unsigned i = 0; i < dotProducts.size(); i += skip) {
+					dotProducts[i] = Math::lerp(dotProducts[i], dotProducts[i + t], lerpFactors[f]);
+				}
+			}
 			
 			return -1.f;
 		}

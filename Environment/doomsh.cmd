ef74a61a56f30f9ab1dfeb53b87a7a57941a1f51
Jakub Neruda <neruda@netcope.com>
2019-11-26 15:44:05 +0100

    [FIX] Paths in doomsh.cmd

--- Environment/doomsh.cmd
+++ Environment/doomsh.cmd
@@ -11,14 +11,14 @@ doskey cp=copy $*
 doskey diff=fc $*
 doskey gmake=cmake.exe -G "MinGW Makefiles" $*
 
-doskey ssh="/tools/various/putty.exe" -ssh $*
-doskey scp="/tools/various/pscp.exe" $*
+doskey ssh="/tools/bin/putty/putty.exe" -ssh $*
+doskey scp="/tools/bin/putty/pscp.exe" $*
 
-set PATH=%PATH%;/tools/utils/bin;/tools/utils/lib;/tools/utils/include
+set PATH=%PATH%;/tools/bin;/tools/utils/lib;/tools/utils/include
 set PATH=%PATH%;/tools/bin/mingw/bin
 set PATH=%PATH%;/tools/bin/doxygen
 set PATH=%PATH%;/tools/bin/python3
-set PATH=%PATH%;/tools/bin/putty/tools/python3
+set PATH=%PATH%;/tools/bin/putty
 set PATH=%PATH%;/tools/bin/miktex/miktex/bin/x64/
 set PATH=%PATH%;/tools/bin/cmake/bin
 

2f63b7f1e4222efa24f91e63d0af339dcf8c70c8
Jakub Neruda <nerudaj@email.cz>
2023-02-24 18:06:21 +0100

    Improved exception message when trying to write too big string in LevelD

--- Libraries/LevelD/Bytestream/BytestreamOut.cpp
+++ Libraries/LevelD/Bytestream/BytestreamOut.cpp
@@ -5,87 +5,100 @@
 
 const std::size_t ALLOC_CHUNK_SIZE = 1024;
 
-void BytestreamOut::updateBufferSize(std::size_t bytecount) {
-    if (pos + bytecount > data.size()) {
-        const auto ALLOC_SIZE = std::max(bytecount, ALLOC_CHUNK_SIZE);
-        data.resize(data.size() + ALLOC_SIZE);
-    }
+void BytestreamOut::updateBufferSize(std::size_t bytecount)
+{
+	if (pos + bytecount > data.size())
+	{
+		const auto ALLOC_SIZE = std::max(bytecount, ALLOC_CHUNK_SIZE);
+		data.resize(data.size() + ALLOC_SIZE);
+	}
 }
 
-BytestreamOut &BytestreamOut::operator<<(uint8_t num) {
-    updateBufferSize(sizeof(num));
+BytestreamOut& BytestreamOut::operator<<(uint8_t num)
+{
+	updateBufferSize(sizeof(num));
 
-    data[pos] = num;
-    pos += sizeof(num);
+	data[pos] = num;
+	pos += sizeof(num);
 
-    return *this;
+	return *this;
 }
 
-BytestreamOut &BytestreamOut::operator<<(uint16_t num) {
-    updateBufferSize(sizeof(num));
+BytestreamOut& BytestreamOut::operator<<(uint16_t num)
+{
+	updateBufferSize(sizeof(num));
 
-    *((uint16_t*)(data.data() + pos)) = sanitize(num);
-    pos += sizeof(num);
+	*((uint16_t*)(data.data() + pos)) = sanitize(num);
+	pos += sizeof(num);
 
-    return *this;
+	return *this;
 }
 
-BytestreamOut &BytestreamOut::operator<<(uint32_t num) {
-    updateBufferSize(sizeof(num));
+BytestreamOut& BytestreamOut::operator<<(uint32_t num)
+{
+	updateBufferSize(sizeof(num));
 
-    *((uint32_t*)(data.data() + pos)) = sanitize(num);
-    pos += sizeof(num);
+	*((uint32_t*)(data.data() + pos)) = sanitize(num);
+	pos += sizeof(num);
 
-    return *this;
+	return *this;
 }
 
-BytestreamOut &BytestreamOut::operator<<(uint64_t num) {
-    updateBufferSize(sizeof(num));
+BytestreamOut& BytestreamOut::operator<<(uint64_t num)
+{
+	updateBufferSize(sizeof(num));
 
-    *((uint64_t*)(data.data() + pos)) = sanitize(num);
-    pos += sizeof(num);
+	*((uint64_t*)(data.data() + pos)) = sanitize(num);
+	pos += sizeof(num);
 
-    return *this;
+	return *this;
 }
 
-BytestreamOut &BytestreamOut::operator<<(const std::string &str) {
-    if (str.size() > 255) {
-        throw std::runtime_error("Trying to export std::string longer than 255 symbols");
-    }
+BytestreamOut& BytestreamOut::operator<<(const std::string& str)
+{
+	if (str.size() > 255)
+	{
+		throw std::runtime_error(
+			"Trying to export std::string longer than 255 symbols. String: " + str);
+	}
 
-    *this << uint8_t(str.size());
+	*this << uint8_t(str.size());
 
-    updateBufferSize(str.size());
+	updateBufferSize(str.size());
 
-    for (unsigned i = 0;  i < str.size(); i++) {
-        data[pos++] = str[i];
-    }
+	for (unsigned i = 0; i < str.size(); i++)
+	{
+		data[pos++] = str[i];
+	}
 
-    return *this;
+	return *this;
 }
 
-BytestreamOut &BytestreamOut::operator<<(const std::vector<uint16_t> &vec) {
-    *this << uint32_t(vec.size());
+BytestreamOut& BytestreamOut::operator<<(const std::vector<uint16_t>& vec)
+{
+	*this << uint32_t(vec.size());
 
-    updateBufferSize(vec.size() * sizeof(uint16_t));
+	updateBufferSize(vec.size() * sizeof(uint16_t));
 
-    uint16_t *vecdata = (uint16_t*)(data.data() + pos);
+	uint16_t* vecdata = (uint16_t*)(data.data() + pos);
 
-    for (unsigned i = 0; i < vec.size(); i++) {
-        vecdata[i] = sanitize(vec[i]);
-    }
+	for (unsigned i = 0; i < vec.size(); i++)
+	{
+		vecdata[i] = sanitize(vec[i]);
+	}
 
-    pos += vec.size() * sizeof(uint16_t);
+	pos += vec.size() * sizeof(uint16_t);
 
-    return *this;
+	return *this;
 }
 
 
-void BytestreamOut::close() {
-    std::ofstream save(filename, std::ios::binary);
+void BytestreamOut::close()
+{
+	std::ofstream save(filename, std::ios::binary);
 
-    save.write((char*)data.data(), pos);
+	save.write((char*)data.data(), pos);
 
-    save.close();
-    save.clear();
+	save.close();
+	save.clear();
 }
